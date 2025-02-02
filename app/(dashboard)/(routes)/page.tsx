8160c55cb329dbdbc87e5ae0dd531d36ca041537
Pritom Kalita <101991870+Tobibaka@users.noreply.github.com>
2025-02-02 15:49:31 +0530

    Authorization

---
+++ app/(dashboard)/(routes)/page.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+  <div className="text-2xl font-medium text-sky-700">
+      <p>
+      Hello World</p>
+      <button >Click me</button>
+
+  </div>
+   
+)
+
+  
+}
