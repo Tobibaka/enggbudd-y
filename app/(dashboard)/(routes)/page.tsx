601dca5de76cd3c8c59c4f7cbd2b33365b790d38
Pritom Kalita <101991870+Tobibaka@users.noreply.github.com>
2025-02-02 16:18:44 +0530

    dashboard redirect

--- app/(dashboard)/(routes)/page.tsx
+++ app/(dashboard)/(routes)/page.tsx
@@ -1,15 +1,15 @@
 import Image from "next/image";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-  <div className="text-2xl font-medium text-sky-700">
-      <p>
-      Hello World</p>
-      <button >Click me</button>
-
+  <div>
+    <UserButton
+      afterSignOutUrl="/"
+    />
   </div>
    
-)
+  )
 
   
 }
