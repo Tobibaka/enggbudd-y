b56fa0e3587acea6dadc596686074fc4a14feef8
Pritom Kalita <101991870+Tobibaka@users.noreply.github.com>
2025-02-08 19:15:16 +0530

    sidebar-layout-link[F]

---
+++ app/(dashboard)/_components/sidebar.tsx
@@ -0,0 +1,16 @@
+export const Sidebar = () => {
+
+    return (
+        <div className="h-full w-56 bg-gray-800">AAAAfdfv
+            <div className="h-16 bg-gray-900 flex items-center justify-center">BBB
+                <div className="h-10 w-10 rounded-full bg-gray-300">bxcvbCCCC</div>
+            </div>
+            <div className="h-full flex flex-col items-center justify-center">
+                <div className="h-10 w-10 rounded-full bg-gray-300">fchdfhdh</div>
+                <div className="h-10 w-10 rounded-full bg-gray-300">jghchgghm</div>
+                <div className="h-10 w-10 rounded-full bg-gray-300">f hthhztgh</div>
+                <div className="h-10 w-10 rounded-full bg-gray-300">fhdfhd</div>
+            </div>
+        </div>
+    )
+}
