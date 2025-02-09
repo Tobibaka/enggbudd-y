cba14750b7f9c89c13348b26d10a31bb3b581c40
Pritom Kalita <101991870+Tobibaka@users.noreply.github.com>
2025-02-10 00:20:00 +0530

    sidebar setup

--- app/(dashboard)/_components/sidebar.tsx
+++ app/(dashboard)/_components/sidebar.tsx
@@ -1,15 +1,17 @@
+"use client";
+
+import { Logo } from ".//logo"
+import { SidebarRoutes } from ".//sidebar-routes"
+
 export const Sidebar = () => {
 
     return (
-        <div className="h-full w-56 bg-gray-800">AAAAfdfv
-            <div className="h-16 bg-gray-900 flex items-center justify-center">BBB
-                <div className="h-10 w-10 rounded-full bg-gray-300">bxcvbCCCC</div>
+        <div className={`h-full border-r flex flex-col overflow-y-auto bg-white text-gray-900 shadow-sm dark:bg-gray-900 dark:text-white`}>
+        <div className="p-6">
+                <Logo />
             </div>
-            <div className="h-full flex flex-col items-center justify-center">
-                <div className="h-10 w-10 rounded-full bg-gray-300">fchdfhdh</div>
-                <div className="h-10 w-10 rounded-full bg-gray-300">jghchgghm</div>
-                <div className="h-10 w-10 rounded-full bg-gray-300">f hthhztgh</div>
-                <div className="h-10 w-10 rounded-full bg-gray-300">fhdfhd</div>
+            <div className="flex flex-col w-full">
+                <SidebarRoutes />
             </div>
         </div>
     )
