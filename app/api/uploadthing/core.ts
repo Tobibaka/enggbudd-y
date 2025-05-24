fc2dae56f16bad67a1f1a767d87f7da9cd8ba134
Pritom Kalita <101991870+Tobibaka@users.noreply.github.com>
2025-05-24 20:44:19 +0530

    upload thing done baby girllllllll wohoooo

--- app/api/uploadthing/core.ts
+++ app/api/uploadthing/core.ts
@@ -15,25 +15,25 @@ export const ourFileRouter = {
         .middleware(async () => await handleAuth())
         .onUploadComplete(async ({ metadata, file }) => {
             console.log("Profile image upload complete for userId:", metadata.userId);
-            console.log("file url", file.url);
+            console.log("file url", file.ufsUrl);
         }),
     courseImage: f({ image: { maxFileSize: "4MB", maxFileCount: 1 } })
         .middleware(async () => await handleAuth())
         .onUploadComplete(async ({ metadata, file }) => {
             console.log("Course image upload complete for userId:", metadata.userId);
-            console.log("file url", file.url);
+            console.log("file url", file.ufsUrl);
         }),
     courseAttachment: f(["text", "image", "video", "audio", "pdf"])
         .middleware(async () => await handleAuth())
         .onUploadComplete(async ({ metadata, file }) => {
             console.log("Course attachment upload complete for userId:", metadata.userId);
-            console.log("file url", file.url);
+            console.log("file url", file.ufsUrl);
         }),
     chapterVideo: f({ video: { maxFileCount: 1, maxFileSize: "512GB" } })
         .middleware(async () => await handleAuth())
         .onUploadComplete(async ({ metadata, file }) => {
             console.log("Chapter video upload complete for userId:", metadata.userId);
-            console.log("file url", file.url);
+            console.log("file url", file.ufsUrl);
         }),
 } satisfies FileRouter;
 
