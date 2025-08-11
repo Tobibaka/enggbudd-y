f88ecf86000739f85f02a76cf680f936797f7094
Pritom Kalita <101991870+Tobibaka@users.noreply.github.com>
2025-08-12 03:03:40 +0530

    refactor: simplify global prisma assignment in db.ts

--- lib/db.ts
+++ lib/db.ts
@@ -9,10 +9,9 @@ import { PrismaClient } from '@prisma/client';
 
 declare global {
     var prisma: PrismaClient | undefined;
-}
+};
 
 export const db = globalThis.prisma || new PrismaClient();
 
-if (process.env.NODE_ENV === 'development') {
-  globalThis.prisma = db;
-}
+if (process.env.NODE_ENV === 'development') globalThis.prisma = db;
+
